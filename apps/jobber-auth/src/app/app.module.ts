931b487edffb6c4084f6b0b7c22ac11736a27833
Artur <A.Ganiullin@gazprom-auto.ru>
2025-07-18 09:15:17 +0300

    feat(tests): auth module added

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthResolver } from './auth/auth.resolver';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { ConfigModule } from '@nestjs/config';
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
+  providers: [AuthResolver],
 })
 export class AppModule {}
